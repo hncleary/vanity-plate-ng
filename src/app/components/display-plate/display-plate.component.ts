fb2744066c4b2ba4c8dc1d698fb63eae1608c038
Hunter Cleary <34077789+hncleary@users.noreply.github.com>
2023-12-07 07:30:21 -0600

    Decreased image size

--- src/app/components/display-plate/display-plate.component.ts
+++ src/app/components/display-plate/display-plate.component.ts
@@ -9,7 +9,7 @@ export class DisplayPlateComponent {
   @Input() public headerText = 'VanityPlate';
   @Input() public displayText = 'Testing';
   public imgSrc = 'assets/blank-plate.png';
-  public plateLeft = 'assets/blank-plate-left.png';
-  public plateMiddle = 'assets/blank-plate-middle.png';
-  public plateRight = 'assets/blank-plate-right.png';
+  public plateLeft = 'assets/blank-plate-left-small.png';
+  public plateMiddle = 'assets/blank-plate-middle-small.png';
+  public plateRight = 'assets/blank-plate-right-small.png';
 }
